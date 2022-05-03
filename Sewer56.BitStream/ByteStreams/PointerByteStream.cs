0512cd85a4894ad541f4ea557c40468acbfcf22d
Sewer 56 <seweratwork@gmail.com>
2022-05-04 00:10:45 +0100

    Added: PointerByteStream to Main Library

---
+++ Sewer56.BitStream/ByteStreams/PointerByteStream.cs
@@ -0,0 +1,15 @@
+﻿using Sewer56.BitStream.Interfaces;
+
+namespace Sewer56.BitStream.ByteStreams
+{
+    /// <summary>
+    /// A byte stream representing a native array.
+    /// </summary>
+    public readonly unsafe struct PointerByteStream : IByteStream
+    {
+        public byte* ArrayPtr { get; }
+        public PointerByteStream(byte* arrayPtr) => ArrayPtr = arrayPtr;
+        public byte Read(int index) => ArrayPtr[index];
+        public void Write(byte value, int index) => ArrayPtr[index] = value;
+    }
+}
