8ec3fd2b5578c107f62f3ef9837261d8385b6ad1
Sewer 56 <seweratwork@gmail.com>
2022-05-03 23:45:11 +0100

    Added: .NET 5.0 Target & Basic Read Benchmark

---
+++ Sewer56.BitStream.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Sewer56.BitStream.Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ReadBenchmark>();
+            //BenchmarkRunner.Run<WriteBenchmark>();
+        }
+    }
+}
---
+++ Sewer56.BitStream.Benchmarks/ReadBenchmark.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Sewer56.BitStream.ByteStreams;
+
+namespace Sewer56.BitStream.Benchmarks
+{
+    [SimpleJob(RuntimeMoniker.CoreRt31)]
+    [SimpleJob(RuntimeMoniker.CoreRt50)]
+    public class ReadBenchmark
+    {
+        private const int NumBytes = 10000;
+        private readonly byte[] _data;
+
+        public ReadBenchmark()
+        {
+            _data = new byte[NumBytes];
+            new Random().NextBytes(_data);
+        }
+
+        [Benchmark]
+        public int ReadBit()
+        {
+            var maxNumIterations = NumBytes * 8;
+            var numIterations    = 0;
+            var stream           = new ArrayByteStream(_data); 
+            var bitStream        = new BitStream<ArrayByteStream>(stream, 0);
+
+            for (; numIterations < maxNumIterations; numIterations++)
+            {
+                bitStream.ReadBit();
+            }
+
+            return numIterations;
+        }
+
+        [Benchmark]
+        public int Read8()
+        {
+            var maxNumIterations = NumBytes;
+            var numIterations = 0;
+            var stream = new ArrayByteStream(_data);
+            var bitStream = new BitStream<ArrayByteStream>(stream, 0);
+
+            for (; numIterations < maxNumIterations; numIterations++)
+            {
+                bitStream.Read8(8);
+            }
+
+            return numIterations;
+        }
+
+        [Benchmark]
+        public int Read16()
+        {
+            var maxNumIterations = NumBytes / 2;
+            var numIterations = 0;
+            var stream = new ArrayByteStream(_data);
+            var bitStream = new BitStream<ArrayByteStream>(stream, 0);
+
+            for (; numIterations < maxNumIterations; numIterations++)
+            {
+                bitStream.Read16(16);
+            }
+
+            return numIterations;
+        }
+
+        [Benchmark]
+        public int Read32()
+        {
+            var maxNumIterations = NumBytes / 4;
+            var numIterations = 0;
+            var stream = new ArrayByteStream(_data);
+            var bitStream = new BitStream<ArrayByteStream>(stream, 0);
+
+            for (; numIterations < maxNumIterations; numIterations++)
+            {
+                bitStream.Read32(32);
+            }
+
+            return numIterations;
+        }
+
+        [Benchmark]
+        public int Read64()
+        {
+            var maxNumIterations = NumBytes / 8;
+            var numIterations = 0;
+            var stream = new ArrayByteStream(_data);
+            var bitStream = new BitStream<ArrayByteStream>(stream, 0);
+
+            for (; numIterations < maxNumIterations; numIterations++)
+            {
+                bitStream.Read64(64);
+            }
+            
+            return numIterations;
+        }
+
+        [Benchmark]
+        public int Read8Generic()
+        {
+            var maxNumIterations = NumBytes;
+            var numIterations = 0;
+            var stream = new ArrayByteStream(_data);
+            var bitStream = new BitStream<ArrayByteStream>(stream, 0);
+
+            for (; numIterations < maxNumIterations; numIterations++)
+            {
+                bitStream.Read<byte>();
+            }
+
+            return numIterations;
+        }
+    }
+}
