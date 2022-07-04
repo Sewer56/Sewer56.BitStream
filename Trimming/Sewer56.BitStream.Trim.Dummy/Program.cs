b404719699f03bb4fc77c970390bb5ba3ce0c4db
Sewer 56 <seweratwork@gmail.com>
2022-07-04 23:13:02 +0100

    Added: Assembly as Trimmable & Trim Test Dummy

---
+++ Trimming/Sewer56.BitStream.Trim.Dummy/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// I'm a dummy for assembly trimming, as the analyzer isn't yet perfect. 
+// Publish me with `dotnet publish -c Release -r win-x64` and check for link warnings. 
+// Please use latest .NET Preview SDK for best results.
+
+Console.WriteLine("Hello, World!");
